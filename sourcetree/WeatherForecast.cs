cfac0b550b82ed033985fd124eeb6e98327bdc74
DESKTOP-AUDUNEO\TCS <mhinaumer@gmail.com>
2023-03-01 09:46:49 +0500

    insert field

--- sourcetree/WeatherForecast.cs
+++ sourcetree/WeatherForecast.cs
@@ -9,5 +9,8 @@ namespace sourcetree
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+        public string? Enabled { get; set; }    
+        public DateTime? Humidity { get; set; } = DateTime.MinValue;
+
     }
 }
